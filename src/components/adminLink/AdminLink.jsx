f05bb476a2d5d5a469bb0ae7cdbc034ae27212df
Yash Solanki <thisisyashs@gmail.com>
2024-02-11 16:44:01 +0530

    Update

--- src/components/adminLink/AdminLink.jsx
+++ src/components/adminLink/AdminLink.jsx
@@ -1,18 +1,18 @@
-const getUser = async () => {
+const getData = async () => {
   const res = await fetch(`http://localhost:3000/api/user`, {
     cache: "no-store",
   });
-  console.log("res", res);
+
   if (!res.ok) {
     throw new Error("Failed");
   }
-
   return res.json();
 };
 
-const AdminLink = () => {
+const AdminLink = async () => {
   //   const user = await getUser();
-  return <div>ISADMdIN2</div>;
+  const data = await getData();
+  return <div> {data?.email} is Admin</div>;
 };
 
 export default AdminLink;
