88b10360685605952c03970bc81998fdc1fc0c2e
Yash Solanki <thisisyashs@gmail.com>
2024-02-20 13:55:42 +0530

    frontend: About section

--- src/components/aboutSection/AboutSection.jsx
+++ src/components/aboutSection/AboutSection.jsx
@@ -1,7 +1,54 @@
+import Image from "next/image";
+import Button from "../button/Button";
+import Highlighter from "../highlighter/Highlighter";
+import VectorComponent from "../vector/Vector";
+
 function AboutSection() {
   return (
-    <section id="about" className="border flex gap-4 h-screen">
-      About Section
+    <section id="about" className="border flex gap-4 h-screen relative">
+      <div className="border flex flex-auto flex-col w-[45%] justify-start items-start p-24 relative">
+        <Highlighter size={3} className="top-1/2 left-1/2 -translate-x-1/2" />
+        <div
+          className=" h-3/5 w-3/5 absolute flex items-center rounded-lg shadow-lg hover:shadow-brand_primary_dark  hover:scale-105 transition-all duration-700
+        "
+        >
+          <Image
+            src="/images/matrix.png"
+            alt="Picture of a matrix"
+            className="w-full h-full object-cover rounded-lg"
+            width={400}
+            height={400}
+          />
+        </div>
+        <div className=" h-3/5 w-3/5 absolute bottom-24 right-24 rounded-lg shadow-lg hover:shadow-brand_primary_dark hover:scale-105 transition-all duration-700">
+          <Image
+            src="/images/typing.png"
+            alt="Picture of a matrix"
+            className="w-full h-full object-cover rounded-lg"
+            width={400}
+            height={400}
+          />
+        </div>
+      </div>
+      <div className="border flex flex-col flex-auto w-[55%] justify-start items-start p-24">
+        <div className="h-3/4 w-3/4">
+          <h4 className="text-brand_primary text-xl">About us</h4>
+          <h2 className="text-7xl font-semibold">
+            Discover Our Journey Protecting Your Digital World with Expertise
+            and Care
+          </h2>
+          <p className="mt-10 text-xl ">
+            At CyberShakha, we are not just a consultancy; we are your partners
+            in digital security. Our dedication to excellence, personalized
+            approach, and a commitment to staying at the forefront of
+            cybersecurity make us the trusted choice for individuals and
+            businesses alike.
+          </p>
+        </div>
+        <Button className="mt-4">Read More</Button>
+      </div>
+      <VectorComponent size={5} className="bottom-0 -right-[500px]" />
+      <Highlighter size={3} className="bottom-0 -right-[500px]" />
     </section>
   );
 }
