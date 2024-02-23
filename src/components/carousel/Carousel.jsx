d3963feccab285a91915964a6df64d75cd3f7a9a
Yash Solanki <thisisyashs@gmail.com>
2024-02-23 22:20:13 +0530

    frontend: Goal Section done

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,121 @@
+"use client";
+import { useState, useRef, useEffect } from "react";
+
+// Data
+import data from "./data.json";
+
+const Carousel = () => {
+  const maxScrollWidth = useRef(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carousel = useRef(null);
+
+  const movePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1);
+    }
+  };
+
+  const moveNext = () => {
+    if (
+      carousel.current !== null &&
+      carousel.current.offsetWidth * currentIndex <= maxScrollWidth.current
+    ) {
+      setCurrentIndex((prevState) => prevState + 1);
+    }
+  };
+
+  const isDisabled = (direction) => {
+    if (direction === "prev") {
+      return currentIndex <= 0;
+    }
+
+    if (direction === "next" && carousel.current !== null) {
+      return (
+        carousel.current.offsetWidth * currentIndex >= maxScrollWidth.current
+      );
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    if (carousel !== null && carousel.current !== null) {
+      carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
+    }
+  }, [currentIndex]);
+
+  useEffect(() => {
+    maxScrollWidth.current = carousel.current
+      ? carousel.current.scrollWidth - carousel.current.offsetWidth
+      : 0;
+  }, []);
+
+  return (
+    <div className="carousel my-12 mx-auto h-full w-full relative overflow-hidden">
+      <div className="flex justify-between absolute top left w-full h-full">
+        <button
+          onClick={movePrev}
+          className="hover:bg-blue-900/75 text-white w-10 h-full text-center opacity-75 hover:opacity-100 disabled:opacity-25 disabled:cursor-not-allowed z-10 p-0 m-0 transition-all ease-in-out duration-300"
+          disabled={isDisabled("prev")}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-12 w-20 -ml-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+          <span className="sr-only">Prev</span>
+        </button>
+        <button
+          onClick={moveNext}
+          className="hover:bg-blue-900/75 text-white w-10 h-full text-center opacity-75 hover:opacity-100 disabled:opacity-25 disabled:cursor-not-allowed z-10 p-0 m-0 transition-all ease-in-out duration-300"
+          disabled={isDisabled("next")}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-12 w-20 -ml-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+          <span className="sr-only">Next</span>
+        </button>
+      </div>
+      <div
+        ref={carousel}
+        className=" h-full w-full carousel-container relative flex px-4 items-center gap-10 overflow-hidden scroll-smooth snap-x snap-mandatory touch-pan-x z-0"
+      >
+        {data.resources.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="carousel-item text-left relative p-10 w-full h-[200px] snap-center bg-glass-gradient rounded-3xl hover:scale-105 transition-all duration-300 border border-transparent hover:border-brand_primary"
+            >
+              <div className="h-44 w-[350px] z-0">
+                <h2 className="text-2xl font-semibold">{item.title}</h2>
+                <p className="mt-4">{item.description}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/goalSection/GoalSection.jsx
+++ src/components/goalSection/GoalSection.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import Carousel from "../carousel/Carousel";
 
 function GoalSection() {
   return (
-    <section id="goal" className="border flex gap-4 h-screen">
-      Goal Section
+    <section id="goal" className=" flex flex-col h-screen mt-24">
+      <div className=" h-1/2 py-10 flex justify-center">
+        <div className=" h-full w-1/2 text-center">
+          <h4 className="text-brand_primary text-xl">Our Goals</h4>
+          <h2 className="text-7xl font-semibold">
+            Securing Your Digital World Together
+          </h2>
+          <p className="mt-10 text-xl">
+            At CyberShakha, we are not just a consultancy; we are your partners
+            in digital security. Our dedication to excellence, personalized
+            approach, and a commitment to staying at the forefront of
+            cybersecurity make us the trusted choice for individuals and
+            businesses alike.
+          </p>
+        </div>
+      </div>
+      {/* <div className="border h-1/2">
+        <Carousel />
+      </div> */}
+      <div className="px-24 py-10">
+        <Carousel />
+      </div>
     </section>
   );
 }
