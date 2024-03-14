2db763a7324e2d2fef6c0831f21dda6e9e13fae2
Yash Solanki <thisisyashs@gmail.com>
2024-03-14 21:10:08 +0530

    frontend : Single Blog Page (w/o Comments)

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import Menu from "@/components/Menu/Menu";
 // import styles from "./singlePage.module.css";
 import Image from "next/image";
+import Link from "next/link";
 // import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
@@ -21,37 +22,67 @@ const SinglePage = async ({ params }) => {
   const data = await getData(slug);
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1>{data?.title}</h1>
-          <div>
+    <div className=" lg:pt-64 pt-52">
+      {/* BLOG HEADER */}
+      <div className=" min-h-[60vh] flex lg:flex-row flex-col pb-10 lg:gap-10 border-b">
+        <div className="lg:h-full   flex flex-col justify-around  lg:w-1/2 lg:pr-20 lg:py-10">
+          {/* TITLE and DESC*/}
+          <h1 className="2xl:text-8xl lg:text-6xl md:text-8xl sm:text-6xl text-5xl md:text-left text-center font-bold">
+            {data?.title}
+          </h1>
+          <div
+            className="text-2xl font-light mt-10 md:text-left text-center"
+            dangerouslySetInnerHTML={{ __html: data?.desc.substring(0, 150) }}
+          />
+          {/* Author Info */}
+          <div className="flex  items-center  lg:mt-20 mt-10">
             {data?.user?.image && (
               <div>
-                <Image src={data.user.image} alt="" fill />
+                <Image
+                  className="rounded-full"
+                  src={data.user.image}
+                  alt=""
+                  width={75}
+                  height={75}
+                />
               </div>
             )}
-            <div>
-              <span>{data?.user.name}</span>
-              <span>01.01.2024</span>
+            <div className="ml-4 flex flex-col">
+              <span className="text-2xl">{data?.user.name}</span>
+              <span className="font-light">01.01.2024</span>
+            </div>
+            <div className="ml-4 px-6 py-2 bg-gray-700 capitalize rounded-lg">
+              {data?.catSlug}
             </div>
           </div>
         </div>
-        {data?.img && (
-          <div>
-            <Image src={data.img} alt="" fill />
-          </div>
-        )}
+        {/* IMG */}
+        <div className=" h-full flex flex-col items-start lg:w-1/2  lg:mt-0 mt-10">
+          <Image
+            src={data?.img}
+            alt="Hero Image"
+            className="w-full aspect-video object-cover shadow-2xl hover:shadow-white  rounded-tl-[15%] rounded-br-[15%] transition-all duration-500 ease-in-out"
+            objectFit="cover"
+            width={500}
+            height={500}
+          />
+        </div>
       </div>
-      <div>
-        <div>
-          <div dangerouslySetInnerHTML={{ __html: data?.desc }} />
-          <div>
+
+      <div className=" w-full flex lg:flex-row flex-col mt-10 lg:gap-10">
+        <div className=" lg:w-2/3 min-h-[50vh] md:pr-10 lg:border-r">
+          <div
+            className="text-lg leading-8 font-light"
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
+          <div className="border-t mt-20 pt-20">
             {/* <Comments postSlug={slug} /> */}
             Comments ....
           </div>
         </div>
-        <Menu />
+        <div className=" lg:mt-0 mt-10 flex lg:w-1/3 min-h-[50vh] ">
+          <Menu />
+        </div>
       </div>
     </div>
   );
