22b74bcc6f9af9dd9e79ec3b9c21fbf7fcd025bf
Yash Solanki <thisisyashs@gmail.com>
2024-03-23 13:10:37 +0530

    frontend: Email form progress

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,6 +1,31 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+
+const initValues = {
+  name: "",
+  email: "",
+  message: "",
+};
+
+const initState = {
+  values: initValues,
+};
 
 function ContactForm() {
+  const [state, setState] = useState(initState);
+  const { values } = state;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({
+      ...prev,
+      values: {
+        ...prev.values,
+        [name]: value,
+      },
+    }));
+  };
   return (
     <div className="flex flex-col w-full h-full">
       <h4 className="text-xl">Send an automatic email</h4>
@@ -9,6 +34,9 @@ function ContactForm() {
         className="mt-10 py-3 px-5 rounded-[20px] text-xl text-black focus:outline-brand_primary cursor-pointer"
         type="text"
         placeholder="Your name here"
+        name="name"
+        value={values.name}
+        onChange={handleChange}
       />
       <textarea
         className="mt-4 w-full py-3 px-5 text-xl rounded-[20px] text-black  focus:outline-brand_primary  cursor-pointer"
@@ -17,6 +45,8 @@ function ContactForm() {
         id="message"
         cols="30"
         rows="3"
+        value={values.message}
+        onChange={handleChange}
       ></textarea>
       <div className="mt-4 relative  cursor-pointer">
         <input
@@ -25,6 +55,8 @@ function ContactForm() {
           name="email"
           id="email"
           placeholder="Enter your email"
+          value={values.email}
+          onChange={handleChange}
         />
         <Link
           className="bg-brand_primary_dark py-2 px-7 rounded-full absolute top-1/2  -translate-y-1/2 right-2"
